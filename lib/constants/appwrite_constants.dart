0eca48c71239d779fc7209697352042dbdb2674c
stankoo003 <aleksastankovicc.2003@gmail.com>
2023-04-23 15:05:08 +0200

    Added Themes, and assets

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,6 @@
+class AppwriteConstants{
+  static const String databaseId ='6442f8f16eb5227b6a02';
+  static const String projectId ='6442f61d20b6794b2a9c';
+  static const String endPoint ='https://localhost/v1';
+
+}
